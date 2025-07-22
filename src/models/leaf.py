93585573848d5e400aa66f2afcc8f911c2e92d46
Ayoussef31 <ayoussef31@gatech.edu>
2025-07-22 23:26:40 +0300

    adding data augmentation

--- src/models/leaf.py
+++ src/models/leaf.py
@@ -82,7 +82,12 @@ class LeafModel(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.3),
 
-            nn.Linear(256, 256),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+
+            nn.Linear(512, 256),
             nn.BatchNorm1d(256),
             nn.ReLU(),
             nn.Dropout(0.3),
